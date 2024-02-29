a8249062a7a640df32afa717999cba83c291f4a4
JonasKarlsen <jonas.karls@outlokk.com>
2024-02-29 14:09:41 +0100

    Lagt til terskel-test til hentKonti og hjelpemetoder.

--- src/test/java/oslomet/testing/EnhetstestBankController.java
+++ src/test/java/oslomet/testing/EnhetstestBankController.java
@@ -9,10 +9,10 @@ import oslomet.testing.API.BankController;
 import oslomet.testing.DAL.BankRepository;
 import oslomet.testing.Models.Konto;
 import oslomet.testing.Models.Kunde;
+import oslomet.testing.Models.Transaksjon;
 import oslomet.testing.Sikkerhet.Sikkerhet;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -33,6 +33,13 @@ public class EnhetstestBankController {
     @Mock
     // denne skal Mock'es
     private Sikkerhet sjekk;
+
+    @Test
+    public void hentTransaksjoner_loggetInn()   {
+        Transaksjon transaksjon = new Transaksjon(12345, "1234567890", 1234, "15122002", "Vær så god!", "Ja", "0987654321");
+
+        when(sjekk.loggetInn()).thenReturn("01010110523");
+    }
 
     @Test
     public void hentKundeInfo_loggetInn() {
@@ -100,5 +107,65 @@ public class EnhetstestBankController {
         // assert
         assertNull(resultat);
     }
+
+    // Terskeltest på 2 millioner kontoer
+    @Test
+    public void hentKonti_terskel() {
+        ArrayList<Konto> konti = kontoGenerator(2_000_000);
+
+        when(sjekk.loggetInn()).thenReturn("01010110523");
+
+        when(repository.hentKonti(anyString())).thenReturn(konti);
+
+        // act
+        List<Konto> resultat = bankController.hentKonti();
+
+        // assert
+        assertEquals(konti, resultat);
+    }
+
+
+
+    // HJELPE-METODER TIL Å FULLFØRE TESTER
+    private static ArrayList<Konto> kontoGenerator(long antall)   {
+        ArrayList<Konto> kontoer = new ArrayList<>();
+
+        for (int i = 0; i < antall; i++)   {
+            kontoer.add(new Konto(tilfeldigString(11), tilfeldigString(11), tilfeldigInt(5), "Lønnskonto", "NOK", null));
+        }
+
+        return kontoer;
+    }
+
+    // HJELPE-METODER TIL Å FULLFØRE TESTER
+    private static ArrayList<Transaksjon> transaksjonsGenerator(long antall)   {
+        ArrayList<Transaksjon> transaksjoner = new ArrayList<>();
+
+        for (int i = 0; i < antall; i++)   {
+            transaksjoner.add(new Transaksjon(tilfeldigInt(5), tilfeldigString(11), tilfeldigDouble(5), tilfeldigString(8), tilfeldigString(50), tilfeldigString(5), tilfeldigString(11)));
+        }
+
+        return transaksjoner;
+    }
+
+
+    private static String tilfeldigString(int n) {
+        long limit = (long) Math.pow(10, n);
+        long randomNumber = limit + new Random().nextInt((int)limit);
+        return String.valueOf(randomNumber).substring(1);
+    }
+
+    private static int tilfeldigInt(int n) {
+        long limit = (long) Math.pow(10, n);
+        long randomNumber = limit + new Random().nextInt((int)limit);
+        return (int) randomNumber;
+    }
+
+    private static double tilfeldigDouble(int n) {
+        long limit = (long) Math.pow(10, n);
+        long randomNumber = limit + new Random().nextInt((int)limit);
+        return (double) randomNumber;
+    }
+
 }
 
